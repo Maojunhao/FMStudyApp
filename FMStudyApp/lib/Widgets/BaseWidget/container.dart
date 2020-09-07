4b99dc9cc444f093d4f66bcc6b5aa1b7bf7c46cc
毛俊豪 <maojunhao@ceair.com>
2020-09-07 15:26:26 +0800

    修改 container.dart

--- FMStudyApp/lib/Widgets/BaseWidget/container.dart
+++ FMStudyApp/lib/Widgets/BaseWidget/container.dart
@@ -11,45 +11,29 @@ class FMContainerVC extends StatelessWidget {
           ),
           backgroundColor: Colors.lightBlue,
         ),
-        body: _topContainer(),
+        body: _container(),
       ),
     );
   }
 
-  Container _topContainer() {
+  Container _container() {
     return Container(
-      margin: const EdgeInsets.only(left: 70, top: 100, right: 0),
+      // width: 320,
+      // height: 200,
+      margin: const EdgeInsets.only(left: 70, top: 100, right: 30),
       constraints: BoxConstraints.expand(
         width: 320,
         height: 200,
       ),
-      // padding: const EdgeInsets.all(10.0),
-      // alignment: Alignment.center,
+
       decoration: BoxDecoration(
-        // border: Border(
-        //   top: BorderSide(
-        //     width: 3,
-        //     color: Colors.cyan,
-        //   ),
-        //   left: BorderSide(
-        //     width: 3,
-        //     color: Colors.red,
-        //   ),
-        //   right: BorderSide(
-        //     width: 3,
-        //     color: Colors.red,
-        //   ),
-        //   bottom: BorderSide(
-        //     width: 3,
-        //     color: Colors.red,
-        //   ),
-        // ),
         border: Border.all(
           color: Colors.red,
           width: 3,
         ),
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30), bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
-        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(30),
+        // borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30), bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+      //   color: Colors.yellow,
         image: DecorationImage(
           image: NetworkImage('http://tiebapic.baidu.com/forum/w%3D580/sign=a96ca741eafaaf5184e381b7bc5594ed/7ea6a61ea8d3fd1f2643ad5d274e251f95ca5f38.jpg'),
           // centerSlice: Rect.largest,
@@ -58,11 +42,13 @@ class FMContainerVC extends StatelessWidget {
         gradient: LinearGradient(colors: [Colors.white, Colors.yellow], begin: FractionalOffset(0, 0), end: FractionalOffset(0, 1)),
       ),
       transform: Matrix4.rotationZ(0.1),
-      alignment: Alignment.center,
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.all(30),
       child: Text(
         "Container",
         style: TextStyle(
           fontSize: 30,
+          color: Colors.red,
         ),
       ),
     );
